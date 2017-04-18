5b69640d2dbe9ae4c1b4f4872f117cdc879e2ca7
Afshin Mehrabani <afshin.meh@gmail.com>
2017-04-18 22:23:00 +0100

    adding matrix with random elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate assert_approx_eq;
 
+extern crate rand;
+
 mod math;
 mod matrix;
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,26 +1,42 @@
+extern crate rand;
+
+use rand::Rng;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Matrix(Vec<Vec<f64>>);
 
 pub trait MatrixTrait {
-    fn new(m: usize, n: usize) -> Self;
+    fn zero(m: usize, n: usize) -> Self;
+    fn random(m: usize, n: usize) -> Self;
+    fn generate(m: usize, n: usize, f: &Fn() -> f64) -> Self;
 }
 
 impl MatrixTrait for Matrix {
     /// Returns a vector with `m` rows and `n` columns
-    fn new(m: usize, n: usize) -> Matrix {
+    fn generate(m: usize, n: usize, f: &Fn() -> f64) -> Matrix {
         let mut mtx: Vec<Vec<f64>> = Vec::with_capacity(n);
 
         for _ in 0..n {
             let mut row: Vec<f64> = Vec::with_capacity(m);
 
             for _ in 0..m {
-                row.push(0f64);
+                row.push(f());
             }
 
             mtx.push(row);
         }
 
         Matrix(mtx)
+    }
+
+    /// Returns a vector with `m` rows and `n` columns with elements of 0
+    fn zero(m: usize, n: usize) -> Matrix {
+        Matrix::generate(m, n, &|| 0f64)
+    }
+
+    /// Returns a vector with `m` rows and `n` columns with random elements
+    fn random(m: usize, n: usize) -> Matrix {
+        Matrix::generate(m, n, &|| rand::thread_rng().gen_range(0f64, 1f64))
     }
 }
 
@@ -31,6 +47,14 @@ mod tests {
     #[test]
     fn empty_matrix_test() {
         let test = Matrix(vec![vec![0f64, 0f64], vec![0f64, 0f64]]);
-        assert_eq!(Matrix::new(2, 2), test);
+        assert_eq!(Matrix::zero(2, 2), test);
+    }
+
+    #[test]
+    fn random_matrix_test() {
+        let test = Matrix::random(2, 2);
+
+        assert_ne!(test.0[0][0], test.0[0][1]);
+        assert_ne!(test.0[1][0], test.0[1][1]);
     }
 }
