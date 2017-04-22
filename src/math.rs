de0df5ec0d9f3beacde8d1cb7c9321b1b6be6c78
Afshin Mehrabani <afshin.meh@gmail.com>
2017-04-22 15:04:18 +0100

    matrix & math: adding dot and mul

--- src/math.rs
+++ src/math.rs
@@ -1,24 +1,43 @@
-/// Calculates the Sigmoid of input `x`
-pub fn sigmoid(x: f64) -> f64 {
-    1f64 / (1f64 + (-x).exp())
+use std::ops::Mul;
+
+
+/// Vector multiplication
+trait Multiplication<T> {
+    fn dot(&self, x: T) -> Self;
 }
 
-/// Calculates the Derivative Sigmoid of input `x`
-pub fn sigmoid_derivative(x: f64) -> f64 {
-    x * (1f64 - x)
+impl<T> Multiplication<T> for Vec<T> where T: Mul<Output = T> + Copy {
+    fn dot(&self, x: T) -> Vec<T> {
+        self.iter().map(|y| *y * x).collect()
+    }
+}
+
+/// Activation functions
+struct Activation {}
+
+impl Activation {
+    /// Calculates the Sigmoid of input `x`
+    pub fn sigmoid(x: f64) -> f64 {
+        1f64 / (1f64 + (-x).exp())
+    }
+
+    /// Calculates the Derivative Sigmoid of input `x`
+    pub fn sigmoid_derivative(x: f64) -> f64 {
+        x * (1f64 - x)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::Activation;
 
     #[test]
     fn sigmoid_test() {
-        assert_approx_eq!(sigmoid(5f64), 0.9933071490f64);
+        assert_approx_eq!(Activation::sigmoid(5f64), 0.9933071490f64);
     }
 
     #[test]
     fn sigmoid_derivative_test() {
-        assert_approx_eq!(sigmoid_derivative(5f64), -20f64);
+        assert_approx_eq!(Activation::sigmoid_derivative(5f64), -20f64);
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -9,17 +9,21 @@ pub trait MatrixTrait {
     fn zero(m: usize, n: usize) -> Self;
     fn random(m: usize, n: usize) -> Self;
     fn generate(m: usize, n: usize, f: &Fn() -> f64) -> Self;
+    fn rows(&self) -> usize;
+    fn cols(&self) -> usize;
+    fn get(&self, m: usize, n: usize) -> f64;
+    fn dot(&self, b: Matrix) -> Matrix;
 }
 
 impl MatrixTrait for Matrix {
     /// Returns a vector with `m` rows and `n` columns
     fn generate(m: usize, n: usize, f: &Fn() -> f64) -> Matrix {
-        let mut mtx: Vec<Vec<f64>> = Vec::with_capacity(n);
+        let mut mtx: Vec<Vec<f64>> = Vec::with_capacity(m);
 
-        for _ in 0..n {
-            let mut row: Vec<f64> = Vec::with_capacity(m);
+        for _ in 0..m {
+            let mut row: Vec<f64> = Vec::with_capacity(n);
 
-            for _ in 0..m {
+            for _ in 0..n {
                 row.push(f());
             }
 
@@ -36,7 +40,46 @@ impl MatrixTrait for Matrix {
 
     /// Returns a vector with `m` rows and `n` columns with random elements
     fn random(m: usize, n: usize) -> Matrix {
-        Matrix::generate(m, n, &|| rand::thread_rng().gen_range(0f64, 1f64))
+        Matrix::generate(m, n, &|| rand::thread_rng().gen_range(-1f64, 1f64))
+    }
+
+    /// Number of the Matrix rows
+    fn rows(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Number of the Matrix columns
+    fn cols(&self) -> usize {
+        self.0[0].len()
+    }
+
+    /// Returns the element in the position M,N
+    fn get(&self, m: usize, n: usize) -> f64 {
+        assert!(self.rows() >= m && self.cols() >= n);
+
+        self.0[m][n]
+    }
+
+    /// Multiplication with Matrix
+    fn dot(&self, b: Matrix) -> Matrix {
+        assert_eq!(self.rows(), b.cols());
+
+        let mut result: Matrix = Matrix::zero(self.rows(), b.cols());
+
+        for (m, row) in self.0.iter().enumerate() {
+            for n in 0usize..b.cols() {
+                let mut cell_result: f64 = 0f64;
+
+                for row_cell in row {
+                    // row of the first Matrix X col of the second Matrix
+                    cell_result += row_cell * b.get(n, m);
+                }
+
+                result.0[m][n] = cell_result;
+            }
+        }
+
+        result
     }
 }
 
@@ -56,5 +99,24 @@ mod tests {
 
         assert_ne!(test.0[0][0], test.0[0][1]);
         assert_ne!(test.0[1][0], test.0[1][1]);
+    }
+
+    #[test]
+    fn random_matrix_get() {
+        let test = Matrix::random(2, 2);
+
+        assert_approx_eq!(test.get(0, 1), test.0[0][1]);
+        assert_approx_eq!(test.get(1, 0), test.0[1][0]);
+        assert_approx_eq!(test.get(1, 1), test.0[1][1]);
+        assert_approx_eq!(test.get(0, 0), test.0[0][0]);
+    }
+
+    #[test]
+    fn random_mul_test() {
+        let a = Matrix(vec![vec![1f64, 2f64], vec![3f64, 4f64]]);
+        let b = Matrix(vec![vec![2f64, 0f64], vec![1f64, 2f64]]);
+        let result = Matrix(vec![vec![4f64, 4f64], vec![10f64, 8f64]]);
+
+        assert_eq!(a.dot(b), result);
     }
 }
