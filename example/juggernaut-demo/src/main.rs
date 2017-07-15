157f5bdd61db61fbd86dc2733e89d5d27652b71a
Afshin Mehrabani <afshin.meh@gmail.com>
2017-07-15 11:13:28 +0100

    verbose mode = 1;

--- example/juggernaut-demo/src/main.rs
+++ example/juggernaut-demo/src/main.rs
@@ -7,6 +7,8 @@ use juggernaut::activation::Sigmoid;
 use juggernaut::sample::Sample;
 
 fn main() {
+    println!("Juggernaut...");
+
     let dataset = vec![
         Sample::new(vec![0f64, 0f64, 1f64], vec![0f64]),
         Sample::new(vec![0f64, 1f64, 1f64], vec![0f64]),
@@ -18,14 +20,25 @@ fn main() {
         Sample::new(vec![1f64, 0f64, 1f64], vec![0f64])
     ];
 
+    println!("Creating the network...");
+
     let mut test = NeuralNetwork::new(dataset, Sigmoid::new());
 
     // 1st layer = 2 neurons - 3 inputs
     test.add_layer(NeuralLayer::new(2, 3));
+
+    println!("First layer created: 2 neurons 3 inputs");
+
     // 2nd layer = 1 neuron - 2 inputs
     test.add_layer(NeuralLayer::new(1, 2));
 
+    println!("Second layer created: 1 neuron 2 inputs");
+
+    println!("Training (60,000 epochs)...");
+
     test.train(60000);
+
+    println!("Done!!");
 
     let think = test.forward(&think_dataset);
 
