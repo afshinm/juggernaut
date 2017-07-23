bc31bfd22e1c84343a1fc08c04b441b77b6ee66e
Arthur Ice <addtheice@gmail.com>
2017-07-22 18:06:36 -0700

    Added Hyperbolic Tangent activation function

--- src/activation.rs
+++ src/activation.rs
@@ -1,3 +1,5 @@
+use std::f64;
+
 /// Activation functions
 pub trait Activation {
     fn new() -> Self;
@@ -43,11 +45,31 @@ impl Activation for Identity {
     }
 }
 
+pub struct HyperbolicTangent;
+
+impl Activation for HyperbolicTangent {
+    fn new() -> HyperbolicTangent {
+        return HyperbolicTangent;
+    }
+
+    /// Calculates the tanh of input `x`
+    fn calc(&self, x: f64) -> f64 {
+        x.tanh()
+    }
+
+    /// Calculates the Derivative tanh of input `x`
+    fn derivative(&self, x: f64) -> f64 {
+        let tanh_factor = x.tanh();
+        1f64 - (tanh_factor * tanh_factor)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Activation;
     use super::Sigmoid;
     use super::Identity;
+    use super::HyperbolicTangent;
 
     #[test]
     fn sigmoid_test() {
@@ -71,6 +93,18 @@ mod tests {
     fn identity_derivative_test() {
         let activation = Identity::new();
         assert_approx_eq!(activation.derivative(15f64), 1f64);
+    }
+
+    #[test]
+    fn tanh_test() {
+        let activation = HyperbolicTangent::new();
+        assert_approx_eq!(activation.calc(3f64), 0.995054754f64);
+    }
+
+    #[test]
+    fn tanh_derivative_test() {
+        let activation = HyperbolicTangent::new();
+        assert_approx_eq!(activation.derivative(3f64), 0.0098660372f64);
     }
 }
 
