e34f3d69870a656ebf285ba80747f7784914f215
Arthur Ice <addtheice@gmail.com>
2017-07-22 17:48:35 -0700

    Added identity activation function, useful for output layer activation in linear regression problems

--- src/activation.rs
+++ src/activation.rs
@@ -25,10 +25,29 @@ impl Activation for Sigmoid {
     }
 }
 
+pub struct Identity;
+
+impl Activation for Identity {
+    fn new() -> Identity {
+        return Identity;
+    }
+
+    /// Calculates the Identity of input `x`
+    fn calc(&self, x: f64) -> f64 {
+        x
+    }
+
+    /// Calculates the Derivative Identity of input `x`
+    fn derivative(&self, x: f64) -> f64 {
+        1f64
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Activation;
     use super::Sigmoid;
+    use super::Identity;
 
     #[test]
     fn sigmoid_test() {
@@ -41,4 +60,17 @@ mod tests {
         let activation = Sigmoid::new();
         assert_approx_eq!(activation.derivative(5f64), -20f64);
     }
+
+    #[test]
+    fn identity_test() {
+        let activation = Identity::new();
+        assert_approx_eq!(activation.calc(5f64), 5f64);
+    }
+
+    #[test]
+    fn identity_derivative_test() {
+        let activation = Identity::new();
+        assert_approx_eq!(activation.derivative(15f64), 1f64);
+    }
 }
+
