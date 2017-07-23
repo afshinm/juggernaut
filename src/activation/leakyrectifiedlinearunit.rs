85814903a86b8f6bcfc9b7d890f6f4a22d0800fc
Arthur Ice <addtheice@gmail.com>
2017-07-23 12:29:25 -0700

    Created leaky rectified linear unit activation.

---
+++ src/activation/leakyrectifiedlinearunit.rs
@@ -0,0 +1,54 @@
+use activation::Activation;
+
+pub struct LeakyRectifiedLinearUnit {
+    alpha_gradient: f64
+}
+
+impl LeakyRectifiedLinearUnit {
+    pub fn new(alpha: f64) -> LeakyRectifiedLinearUnit {
+        return LeakyRectifiedLinearUnit {
+            alpha_gradient: alpha
+        };
+    }
+}
+
+impl Activation for LeakyRectifiedLinearUnit {
+
+    /// Calculates the LeakyRectifiedLinearUnit of input `x`
+    fn calc(&self, x: f64) -> f64 {
+        if x <= 0f64 {
+            self.alpha_gradient * x
+        } else {
+            x
+        }
+    }
+
+    /// Calculates the Derivative LeakyRectifiedLinearUnit of input `x`
+    fn derivative(&self, x: f64) -> f64 {
+        if x <= 0f64 {
+            self.alpha_gradient
+        } else {
+            x
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Activation;
+    use super::LeakyRectifiedLinearUnit;
+
+    #[test]
+    fn leakyrectifiedlinearunit_test() {
+        let activation = LeakyRectifiedLinearUnit::new(0.01f64);
+        assert_approx_eq!(activation.calc(3.4f64), 3.4f64);
+        assert_approx_eq!(activation.calc(-3.4f64), -0.034f64);
+    }
+
+    #[test]
+    fn leakyrectifiedlinearunit_derivative_test() {
+        let activation = LeakyRectifiedLinearUnit::new(0.01f64);
+        assert_approx_eq!(activation.derivative(-3.4f64), 0.01f64);
+        assert_approx_eq!(activation.derivative(3.4f64), 3.4f64);
+    }
+}
--- src/activation/mod.rs
+++ src/activation/mod.rs
@@ -3,12 +3,14 @@ pub mod identity;
 pub mod hyperbolictangent;
 pub mod softplus;
 pub mod rectifiedlinearunit;
+pub mod leakyrectifiedlinearunit;
 
 pub use self::sigmoid::Sigmoid;
 pub use self::identity::Identity;
 pub use self::hyperbolictangent::HyperbolicTangent;
 pub use self::softplus::SoftPlus;
 pub use self::rectifiedlinearunit::RectifiedLinearUnit;
+pub use self::leakyrectifiedlinearunit::LeakyRectifiedLinearUnit;
 
 /// Activation functions
 pub trait Activation {
