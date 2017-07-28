fb0b3aaf9da40706e1e07265cc12017ee93cbe11
addtheice <addtheice@gmail.com>
2017-07-28 18:22:47 +0000

    fixed activation on layers
    I think that should fix this nicely. This is why I hate modifying an API interface, everyone has issues with it until the new interface 'sticks' in their head. Drives me nuts. Oh well, forward progress! <runs around the room cheering>

--- examples/iris.rs
+++ examples/iris.rs
@@ -7,7 +7,6 @@ use std::fs::File;
 use std::path::PathBuf;
 use juggernaut::nl::NeuralLayer;
 use juggernaut::nn::NeuralNetwork;
-use juggernaut::activation::Activation;
 use juggernaut::activation::Sigmoid;
 use juggernaut::sample::Sample;
 use juggernaut::matrix::MatrixTrait;
@@ -62,15 +61,15 @@ fn main() {
 
     println!("Creating the network...");
 
-    let mut test = NeuralNetwork::new(dataset, Sigmoid::new());
+    let mut test = NeuralNetwork::new(dataset);
 
     // 1st layer = 2 neurons - 3 inputs
-    test.add_layer(NeuralLayer::new(7, 4));
+    test.add_layer(NeuralLayer::new(7, 4, Sigmoid::new()));
 
     println!("First layer created: 2 neurons 3 inputs");
 
     // 2nd layer = 1 neuron - 2 inputs
-    test.add_layer(NeuralLayer::new(3, 7));
+    test.add_layer(NeuralLayer::new(3, 7, Sigmoid::new()));
 
     println!("Second layer created: 1 neuron 2 inputs");
 
