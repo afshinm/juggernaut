5dd9385c55993b832ed0ea69b94288683fbc9cac
Afshin Mehrabani <afshin.meh@gmail.com>
2017-09-04 23:31:01 +0100

    feat(softmax): adding first version of softmax and ActivationVec trait

--- src/activation/mod.rs
+++ src/activation/mod.rs
@@ -2,6 +2,7 @@ pub mod sigmoid;
 pub mod identity;
 pub mod hyperbolictangent;
 pub mod softplus;
+pub mod softmax;
 pub mod rectifiedlinearunit;
 pub mod leakyrectifiedlinearunit;
 
@@ -9,13 +10,24 @@ pub use self::sigmoid::Sigmoid;
 pub use self::identity::Identity;
 pub use self::hyperbolictangent::HyperbolicTangent;
 pub use self::softplus::SoftPlus;
+pub use self::softmax::SoftMax;
 pub use self::rectifiedlinearunit::RectifiedLinearUnit;
 pub use self::leakyrectifiedlinearunit::LeakyRectifiedLinearUnit;
 
-/// Activation functions
+/// Activation trait
 pub trait Activation {
     // the function itself
     fn calc(&self, x: f64) -> f64;
     // Derivative
     fn derivative(&self, x: f64) -> f64;
+}
+
+/// Activation trait for Vec input
+// TODO (afshin): We need to merge these two traits. 
+// This trait is only used for Softmax
+pub trait ActivationVec {
+    // the function itself
+    fn calc(&self, x: Vec<f64>) -> Vec<f64>;
+    // Derivative
+    fn derivative(&self, x: Vec<f64>) -> Vec<f64>;
 }
