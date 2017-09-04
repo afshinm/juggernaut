edce595d6fd4d886ec4dc3685284fe2999fd2847
Afshin Mehrabani <afshin.meh@gmail.com>
2017-09-04 23:48:50 +0100

    fix(softmax): changing the closure to a function pointer

--- src/activation/softmax.rs
+++ src/activation/softmax.rs
@@ -13,7 +13,7 @@ impl SoftMax {
 impl ActivationVec for SoftMax {
     /// Calculates the SoftMax of input `x`
     fn calc(&self, x: Vec<f64>) -> Vec<f64> {
-        let exps = x.iter().map(|x| x.exp());
+        let exps = x.iter().cloned().map(f64::exp as fn(f64) -> f64);
         let exp_sum: f64 = exps.clone().sum();
         exps.map(|x| x / exp_sum).collect::<Vec<f64>>()
     }
@@ -33,7 +33,7 @@ mod tests {
     fn softmax_test() {
         let activation = SoftMax::new();
         let result = activation.calc(vec![1f64, 5f64, 4f64]);
-        let validate = vec![1f64, 1f64, 1f64];
+        let validate = vec![0.013212886f64, 0.72139918427f64, 0.2653879287f64];
 
         for (i, r) in result.iter().enumerate() {
             assert_approx_eq!(r, validate[i]);
